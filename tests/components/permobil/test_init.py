1a872a8cfe769842914473fd894940af99b7dd60
Isak Nyberg <36712644+IsakNyberg@users.noreply.github.com>
2023-07-19 13:05:32 +0000

    remove measurement unit tests
    since the unit used is now the same as HA it no longer needs to be tested

--- tests/components/permobil/test_init.py
+++ tests/components/permobil/test_init.py
@@ -4,10 +4,6 @@ import pytest
 
 from homeassistant.components.permobil import async_setup_entry, async_unload_entry
 from homeassistant.components.permobil.const import DOMAIN
-from homeassistant.const import (
-    CONF_UNIT_OF_MEASUREMENT,
-    UnitOfLength,
-)
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 
@@ -46,10 +42,6 @@ async def test_async_setup_entry_and_unload(hass: HomeAssistant) -> None:
         == config_entry.data["email"]
     )
 
-    # Assert that the unit of distance is set correctly in hass.data
-    assert "unit" in hass.data[DOMAIN]
-    assert hass.data[DOMAIN]["unit"][config_entry.entry_id] == UnitOfLength.KILOMETERS
-
     # Unload the config entry
     assert await async_unload_entry(hass, config_entry)
 
@@ -69,25 +61,6 @@ async def test_async_setup_entry_fail_authenticate(hass: HomeAssistant) -> None:
     # Call the async_setup_entry function
     with pytest.raises(ConfigEntryNotReady):
         await async_setup_entry(hass, config_entry)
-
-
-async def test_async_setup_entry_invalid_unit(hass: HomeAssistant) -> None:
-    """Test for when the api returns an invalid unit of distance."""
-    # Create a mock config entr
-    data = DATA.copy()
-    data[CONF_UNIT_OF_MEASUREMENT] = "invalid"
-    config_entry = MockConfigEntry(
-        domain=DOMAIN,
-        data=data,
-    )
-    config_entry.add_to_hass(hass)
-
-    # Call the async_setup_entry function
-    await async_setup_entry(hass, config_entry)
-
-    # Assert that the unit of distance defaulted to KM
-    assert "unit" in hass.data[DOMAIN]
-    assert hass.data[DOMAIN]["unit"][config_entry.entry_id] == UnitOfLength.KILOMETERS
 
 
 async def test_async_setup_multiple_configs(hass: HomeAssistant) -> None:
@@ -125,12 +98,6 @@ async def test_async_setup_multiple_configs(hass: HomeAssistant) -> None:
         hass.data[DOMAIN]["api"][config_entry2.entry_id].email
         == config_entry2.data["email"]
     )
-
-    # Assert that the unit of distance is set correctly in hass.data
-    assert "unit" in hass.data[DOMAIN]
-    assert hass.data[DOMAIN]["unit"][config_entry1.entry_id] == UnitOfLength.KILOMETERS
-    assert "unit" in hass.data[DOMAIN]
-    assert hass.data[DOMAIN]["unit"][config_entry2.entry_id] == UnitOfLength.MILES
 
     # Unload the config entry
     # Assert that the integration data is removed from the hass.data dictionary
